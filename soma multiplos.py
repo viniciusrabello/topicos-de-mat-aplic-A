13795fe724d24e64c31259707c596601afbc8824
viniciusrabello <48933304+viniciusrabello@users.noreply.github.com>
2019-04-05 20:50:05 -0300

    Add files via upload

---
+++ soma multiplos.py
@@ -0,0 +1,23 @@
+print('\no programa irá calcular a soma dos multiplos menores que n de x ou y\n(multiplos em comum só serão adicionados uma vez)')
+
+x = int(input('\ninsira x: '))
+y = int(input('\ninsira y: '))
+n = int(input('\ninsira n: '))
+lx = []
+ly = []
+l = []
+s = 0
+i = 0
+
+while (i < n):
+    if i % x == 0:
+        lx.append(i)
+    if i % y == 0:
+        ly.append(i)
+    if i % x == 0 and i % y == 0:
+        l.append(i)
+    i += 1
+
+s = sum(lx) + sum (ly) - sum (l)
+print(s)
+        
